48d3451cfb88b3b94308572d7c1625027469ebf1
Nazar <nazarpetrovskyy@gmail.com>
2022-05-04 09:47:45 +0300

    New commit

--- ServerUI/Program.cs
+++ ServerUI/Program.cs
@@ -8,24 +8,26 @@ namespace ServerUI
     {
         static void Main(string[] args)
         {
-            //PeopleInfo peopleInfo = new PeopleInfo();
-            //peopleInfo.Id = 1;
-            //peopleInfo.Name = "Nazar";
-            //peopleInfo.LastName = "zaazaz";
-            //peopleInfo.Age = 20;
+            PeopleInfo peopleInfo = new PeopleInfo();
+            peopleInfo.Id = 1;
+            peopleInfo.Name = "Nazar";
+            peopleInfo.LastName = "zaazaz";
+            peopleInfo.Age = 20;
 
-            //try
-            //{
-            //    DataLibrary dataLibrary = new DataLibrary();
-            //    dataLibrary.Add(peopleInfo);
-            //    dataLibrary.SaveChanges();
+            try
+            {
+                DataLibrary dataLibrary = new DataLibrary();
+                dataLibrary.Add(peopleInfo);
+                dataLibrary.SaveChanges();
 
-            //    Console.WriteLine("Дані записані ");
-            //}
-            //catch(Exception ex)
-            //{
-            //    Console.WriteLine("Помилка: " + ex.Message);
-            //}
+                Console.WriteLine("Дані записані ");
+
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Помилка: " + ex.Message);
+            }
         }
     }
 }

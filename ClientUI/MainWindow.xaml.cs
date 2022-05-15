d42e32272ccea2ddab684dd58e944899779511dd
Encond <dima.pavlyuk81@gmail.com>
2022-05-14 20:11:08 -0400

    Add ClientUI - (MainWindow) Some visual

--- ClientUI/MainWindow.xaml.cs
+++ ClientUI/MainWindow.xaml.cs
@@ -29,6 +29,93 @@ namespace ClientUI
             {
                 this.Close();
             }
+
+            textboxSpeciality.Text = "Doctor";
+            textboxName.Text = "Mark";
+            textboxLastname.Text = "Klarson";
+            textboxAge.Text = "15";
+        }
+
+        private void SetTextBox_IsReadOnly(bool status)
+        {
+            textboxSpeciality.IsReadOnly = status;
+            textboxName.IsReadOnly = status;
+            textboxLastname.IsReadOnly = status;
+            textboxAge.IsReadOnly = status;
+        }
+
+        private void SetUpTextBoxTags()
+        {
+            textboxSpeciality.Tag = textboxSpeciality.Text;
+            textboxName.Tag = textboxName.Text;
+            textboxLastname.Tag = textboxLastname.Text;
+            textboxAge.Tag = textboxAge.Text;
+        }
+
+        private void SetTagsToTextBox()
+        {
+            textboxSpeciality.Text = textboxSpeciality.Tag.ToString();
+            textboxName.Text = textboxName.Tag.ToString();
+            textboxLastname.Text = textboxLastname.Tag.ToString();
+            textboxAge.Text = textboxAge.Tag.ToString();
+        }
+
+        private void ButtonEditVisibility(bool status)
+        {
+            if (status)
+            {
+                buttonEditApply.Visibility = Visibility.Visible;
+                buttonEditCancel.Visibility = Visibility.Visible;
+                buttonEditInfo.IsEnabled = false;
+            }
+            else
+            {
+                buttonEditApply.Visibility = Visibility.Hidden;
+                buttonEditCancel.Visibility = Visibility.Hidden;
+                buttonEditInfo.IsEnabled = true;
+            }
+        }
+
+        private void buttonEditInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+            SetUpTextBoxTags();
+
+            textboxSpeciality.Clear();
+            textboxName.Clear();
+            textboxLastname.Clear();
+            textboxAge.Clear();
+
+            textboxSpeciality.Focus();
+
+            SetTextBox_IsReadOnly(false);
+
+            ButtonEditVisibility(true);
+        }
+
+        private void buttonEditApply_Click(object sender, RoutedEventArgs e)
+        {
+            buttonEditApply.Visibility = Visibility.Hidden;
+            buttonEditCancel.Visibility = Visibility.Hidden;
+            buttonEditInfo.IsEnabled = true;
+
+            SetTextBox_IsReadOnly(true);
+        }
+
+        private void buttonEditCancel_Click(object sender, RoutedEventArgs e)
+        {
+            SetTagsToTextBox();
+            ButtonEditVisibility(false);
+        }
+
+        private void buttonHide_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonHide.Content.ToString() == "Hide")
+                buttonHide.Content = "Show";
+            else
+                buttonHide.Content = "Hide";
+
+            
         }
     }
 }
